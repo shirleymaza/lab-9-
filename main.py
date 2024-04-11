7be015bc3c4cf7494bad26525cdf27c14f79ab2d
shirleymaza <166472849+shirleymaza@users.noreply.github.com>
2024-04-11 16:53:41 -0400

    added menu to my repo

--- main.py
+++ main.py
@@ -22,3 +22,20 @@ def menu():
     print("2. Decode")
     print("3. Quit")
     
+
+def main():
+    while True:
+        menu()
+        option = input("Please enter an option: ")
+        if option == '1':
+            password = input("Please enter your password to encode: ")
+            new = encode(password)
+            print("Your password has been encoded and stored!")
+        elif option == '2':
+            print(f"The encoded password is {new}, and the original password is {password}.")
+        elif option == '3':
+            break
+
+
+if __name__ == "__main__":
+    main()
