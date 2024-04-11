ca4a9d00c52e82b864194291ca5ee7bc072a6d30
katarinaneal <166740315+katarinaneal@users.noreply.github.com>
2024-04-11 16:16:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,7 @@ def encode(password):
         encoded_password += str(int(newchar) % 10)
     return encoded_password
     
-#Katarina Neal(added decoder)
-def decoder(password):
-    result = ''
-    for digit in password:
-        new_digit = str((int(digit) - 3) % 10)
-        result += new_digit
-    return result
+
 
 #my code
 def menu():
