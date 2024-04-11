1b45efa304538a1c27adc4d31732b1296ea1bf11
shirleymaza <166472849+shirleymaza@users.noreply.github.com>
2024-04-11 16:49:34 -0400

    added menu to my repo

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ def encode(password):
         newchar = str(int(char) + 3)
         encoded_password += str(int(newchar) % 10)
     return encoded_password
-    
-#Katarina Neal(added decoder)
+
+
+# Katarina Neal(added decoder)
 def decoder(password):
     result = ''
     for digit in password:
@@ -14,11 +15,29 @@ def decoder(password):
         result += new_digit
     return result
 
-#my code
+
+# my code
 def menu():
     print("Menu")
     print("-------------")
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
-    
+
+
+def main():
+    while True:
+        menu()
+        option = input("Please enter an option: ")
+        if option == '1':
+            password = input("Please enter your password to encode: ")
+            new = encode(password)
+            print("Your password has been encoded and stored!")
+        elif option == '2':
+            print(f"The encoded password is {new}, and the original password is {password}.")
+        elif option == '3':
+            break
+
+
+if __name__ == "__main__":
+    main()
